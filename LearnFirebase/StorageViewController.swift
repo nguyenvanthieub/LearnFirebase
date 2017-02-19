cf7367bc5de7770cc9e1f08814d25a7c329e5197
Thieu Mao <nguyen.van.thieub@framgia.com>
2017-02-19 14:51:13 +0700

    Process Bar Upload

--- LearnFirebase/StorageViewController.swift
+++ LearnFirebase/StorageViewController.swift
@@ -11,25 +11,35 @@ import Firebase
 
 class StorageViewController: UIViewController {
 
+    @IBOutlet weak var processBar: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        processBar.minimumValue = 0
+        processBar.maximumValue = 100
+        
         let storage = FIRStorage.storage()
         let rootFolder = storage.reference(forURL: "gs://learnfirebase-9aebf.appspot.com")
         let audioFolder = rootFolder.child("audios")
         let audioFile = audioFolder.child("lethergo.mp3")
         let path = Bundle.main.path(forResource: "lethergo", ofType: "mp3")
         let url = URL(fileURLWithPath: path!)
-        do {
-            let audioData = try Data(contentsOf: url)
-            audioFile.put(audioData, metadata: nil) { (storeage: FIRStorageMetadata?, error: Error?) in
-                if let error = error {
-                    print(error.localizedDescription)
-                } else {
-                    print(audioData)
-                }
+        
+        let audioData = try! Data(contentsOf: url)
+        let uploadTask = audioFile.put(audioData, metadata: nil) { (storeage: FIRStorageMetadata?, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print(audioData)
             }
-        } catch {
-            print("Vao catch")
+        }
+        
+        uploadTask.observe(.progress) { (snap:FIRStorageTaskSnapshot) in
+            let completed: Float = Float((snap.progress?.completedUnitCount)!)
+            let total: Float = Float((snap.progress?.totalUnitCount)!)
+            let percent: Float = completed / total * 100
+            self.processBar.value = percent
         }
 
     }
